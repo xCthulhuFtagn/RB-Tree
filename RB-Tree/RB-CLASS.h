44197cd4f3bdef71819389f2f06354f5c2766934
xCthulhuFtagn <mkolodyazhny@gmail.com>
2021-08-05 23:33:41 +0300

    added constructor, black by default

--- RB-Tree/RB-CLASS.h
+++ RB-Tree/RB-CLASS.h
@@ -1,6 +1,15 @@
 #pragma once
+#include <iostream>
 template <typename Type>
 class RBNode {
+public:
+	RBNode() {
+		is_red = 0;
+		left = NULL;
+		right = NULL;
+		parent = NULL;
+	}
+private:
 	bool is_red;
 	Type data;
 	RBNode *left, *right, *parent;
