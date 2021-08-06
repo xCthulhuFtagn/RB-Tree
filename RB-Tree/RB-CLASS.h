e49003259d61cd82ffb3afb11a4f5b094db64bc4
Zhmyhevskyi <duzhenko.misha@mail.ru>
2021-08-06 19:02:36 +0300

    Added function "add" and class "RBtree" For function "add" required rotations

--- RB-Tree/RB-CLASS.h
+++ RB-Tree/RB-CLASS.h
@@ -1,16 +1,38 @@
 #pragma once
 #include <iostream>
+
 template <typename Type>
-class RBNode {
+class RBTree;
+
+template <typename Type>
+struct RBNode {
+	bool is_red;
+	unsigned bh; //black height
+	Type data;
+	RBNode* left, * right, * parent;
 public:
-	RBNode() {
-		is_red = 0;
+	RBNode(Type new_data) {
+		data = new_data;
+		bh = 0;
+		is_red = true;
 		left = NULL;
 		right = NULL;
 		parent = NULL;
 	}
-private:
-	bool is_red;
-	Type data;
-	RBNode *left, *right, *parent;
+	template <typename T>
+	friend RBNode<T>* add(RBNode<T>*, T new_elem);
+};
+
+template <typename Type>
+class RBTree {
+	RBNode <Type> root;
+public:
+	RBTree() {
+		root = nullptr;
+	}
+	&RBtree add(Type new_elem) {
+		root = add(root, new_elem);
+		root->is_red = false;
+		return *this;
+	}
 };
--- RB-Tree/main.cpp
+++ RB-Tree/main.cpp
@@ -3,5 +3,5 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Red-black trees!\n";
 }
